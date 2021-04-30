0ae7a54a79bfa289cdb2a0035abb0745ee3b2fda
borninfreedom <z19040042@s.upc.edu.cn>
2021-04-30 16:27:22 +0800

    add rllib spinup

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # deep-rl-with-robots
+Train kuka robot reach a point with deep rl in pybullet.
+
+* **NOTE: The main brach is trained with spinup, and there are some issues with gpu and multi core CPUs at the same time, so this brach will be deprecated in the future. The rllib branch is trained with ray/rllib, and this branch will be mainly used in the future.**
+
+The train process with mlp|The evaluate process with mlp|train plot
+:---------------:|:------------------:|:-------------------------:
+![](https://github.com/borninfreedom/kuka-reach-drl/blob/main/pictures/reach_train_with_mlp.gif)|![](https://github.com/borninfreedom/kuka-reach-drl/blob/main/pictures/reach_result_with_mlp.gif)|![](https://github.com/borninfreedom/kuka-reach-drl/blob/main/pictures/reach_mlp_train_process.png)
+
+The train process with cnn|The evaluate process with cnn|train plot
+:---------------:|:------------------:|:-------------------------:
+![](https://github.com/borninfreedom/kuka-reach-drl/blob/main/pictures/reach_train_with_cnn.gif)|![](https://github.com/borninfreedom/kuka-reach-drl/blob/main/pictures/reach_result_with_cnn.gif)|
+
+
+# Installation guide (Now only support linux and macos)
+* I recommend using conda to install, because conda can help you install cudatoolkit and cudnn, so you don't need to care which version you have installed in your machine, conda helps you install them in an isolated environment.
+
+```bash
+pip install ray,ray[rllib],ray[tune]
+conda install pytorch-gpu
+pip install pybullet
+```
+
+# Run instruction
+
+
+
+# Wiki
+## [Reinforcement Learning](https://github.com/borninfreedom/kuka-reach-drl/blob/rllib/docs/Reinforcement%20Learning.md)
+## [pybullet](https://github.com/borninfreedom/kuka-reach-drl/blob/rllib/docs/pybullet.md)
+## [robotics](https://github.com/borninfreedom/kuka-reach-drl/blob/rllib/docs/robotics.md)
+## [deep learning](https://github.com/borninfreedom/kuka-reach-drl/blob/rllib/docs/deep_learning.md)
+## [python](https://github.com/borninfreedom/kuka-reach-drl/blob/rllib/docs/python.md)
+## [vscode](https://github.com/borninfreedom/kuka-reach-drl/blob/rllib/docs/vscode.md)
+
+
+
+
---
+++ train/kuka_reach/with_value/baselines.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   train.py
+@Time    :   2021/03/20 14:30:42
+@Author  :   Yan Wen 
+@Version :   1.0
+@Contact :   z19040042@s.upc.edu.cn
+@Desc    :   None
+'''
+
+# here put the import lib
+
+import os, inspect
+
+current_dir = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
+os.chdir(current_dir)
+import sys
+
+sys.path.append('../../../')
+
+from custom_env import KukaReachEnv
+from spinningup_algos.core import MLPActorCritic
+from spinningup_algos.ppo import ppo
+from spinup.utils.mpi_tools import mpi_fork
+#import spinningup_algos.core as core
+
+import argparse
+
+parser = argparse.ArgumentParser()
+# parser.add_argument('--env', type=str, default='HalfCheetah-v2')
+
+#modified this to satisfy the custom env
+#parser.add_argument('--env', type=str, default=env)
+parser.add_argument('--is_render', action="store_true")
+parser.add_argument('--is_good_view', action="store_true")
+
+parser.add_argument('--hid', type=int, default=64)
+parser.add_argument('--l', type=int, default=2)
+parser.add_argument('--gamma', type=float, default=0.99)
+parser.add_argument('--seed', '-s', type=int, default=0)
+parser.add_argument('--cpu', type=int, default=5)
+parser.add_argument('--steps', type=int, default=4000)
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--exp_name', type=str, default='ppo-kuka-reach')
+parser.add_argument('--log_dir', type=str, default="../../../logs")
+args = parser.parse_args()
+
+env_config = {
+    "is_render": False,
+    "is_good_view": False,
+    "max_steps_one_episode": 1000
+}
+
+env = KukaReachEnv(env_config)
+
+mpi_fork(args.cpu)  # run parallel code with mpi
+
+from spinup.utils.run_utils import setup_logger_kwargs
+
+logger_kwargs = setup_logger_kwargs(args.exp_name,
+                                    args.seed,
+                                    data_dir=args.log_dir)
+
+ppo(env,
+    actor_critic=MLPActorCritic,
+    ac_kwargs=dict(hidden_sizes=[args.hid] * args.l),
+    gamma=args.gamma,
+    seed=args.seed,
+    steps_per_epoch=1000 * args.cpu,
+    epochs=args.epochs,
+    logger_kwargs=logger_kwargs)
